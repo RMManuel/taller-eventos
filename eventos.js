66a087803535f9b33b3062e8737e8209dd51dfe3
Manuel Romero <romero.marichal@gmail.com>
2023-08-09 14:07:14 -0300

    Agregando funcion Click en div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let container=document.getElementById("div");
+    container.addEventListener("click", function(){
+        alert("Hola! Soy un div");
+    })
+})
