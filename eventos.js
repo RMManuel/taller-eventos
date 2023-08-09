a710d91e3f8ac5e89030c59618d526a8c3d99b22
Manuel Romero <romero.marichal@gmail.com>
2023-08-09 14:17:04 -0300

    Solucion de propagacion de eventos

--- eventos.js
+++ eventos.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     container.addEventListener("click", function(){
         alert("Hola! Soy un div");
     })
+    let btn=document.getElementById("btn")
+    btn.addEventListener("click", function(e){
+        alert("Hola!")
+        e.stopPropagation()
+    })
 })
